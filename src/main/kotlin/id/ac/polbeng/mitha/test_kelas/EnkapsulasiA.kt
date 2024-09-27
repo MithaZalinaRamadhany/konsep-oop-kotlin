3ded52f7d6d14929649e16d68942cf0773ec6960
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-27 12:37:45 +0700

    push stater project

---
+++ src/main/kotlin/id/ac/polbeng/mitha/test_kelas/EnkapsulasiA.kt
@@ -0,0 +1,25 @@
+package id.ac.polbeng.mitha.test_kelas
+//MITHA ZALINA RAMADHANY
+//RPL 5A
+
+class Employee(_id: Int, _name: String, _age: Int) {
+    val id: Int = _id
+        get() = field
+
+    var name: String = _name
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    var age: Int = _age
+        get() = field
+        set(value) {
+            field = value
+        }
+}
+
+fun main() {
+    val emp = Employee(1101, "Jono", 25)
+    println("Id : ${emp.id}, Nama : ${emp.name}, Umur : ${emp.age}")
+}
