ce24e28a4a6ff9e12a7f06c319c9c269be96629b
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-09-27 19:06:06 +0700

    push stater project

---
+++ src/main/kotlin/id/ac/polbeng/mitha/test_kelas/InheritanceE.kt
@@ -0,0 +1,22 @@
+package id.ac.polbeng.mitha.test_kelas
+//MITHA ZALINA RAMADHANY
+//RPL 5A
+
+open class NewPerson {
+    open var age: Int = 1
+}
+
+class CheckedPerson: NewPerson(){
+    override var age: Int = 1
+        set(value) {
+            field = if(value > 0) value else throw IllegalArgumentException("Age can not be negative")
+        }
+}
+
+fun main(args: Array<String>) {
+    val person = NewPerson()
+    person.age = -5 // Works
+
+    val checkedPerson = CheckedPerson()
+    checkedPerson.age = -5 // Throws IllegalArgumentException : Age can not be negative
+}
